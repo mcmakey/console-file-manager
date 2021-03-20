32760c8c790b0e2e6a107c4c5049f2733ce43185
makey <vadimmakievsky@gmail.com>
2021-03-20 13:53:24 +0300

    Begin: Создание класса "FileManager";

---
+++ ConsoleFileManager/FileManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleFileManager
+{
+    class FileManager
+    {
+        public FileManager()
+        {
+
+        }
+
+        public void List()
+        {
+            Console.WriteLine("List");
+        }
+
+        public void CopyFile()
+        {
+            Console.WriteLine("CopyFile");
+        }
+
+        public void CopyDirectory()
+        {
+            Console.WriteLine("CopyDirectory");
+        }
+
+        public void FileInfo()
+        {
+            Console.WriteLine("FileInfo");
+        }
+
+        public void DirInfo()
+        {
+            Console.WriteLine("DirInfo");
+        }
+    }
+}
