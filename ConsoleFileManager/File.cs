54250a25791d56b6f4992d48d345396927e3ebe7
makey <vadimmakievsky@gmail.com>
2021-04-10 15:47:35 +0300

    TreeFrame: Созданы классы FilesTree и Files

---
+++ ConsoleFileManager/File.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleFileManager
+{
+    class File
+    {
+        public string Name { get; }
+        public bool IsDirectory { get; }
+        public int Rank { get; }
+
+        public File(string name, bool isDirectory, int rank)
+        {
+            this.Name = name;
+            this.IsDirectory = isDirectory;
+            this.Rank = rank;
+        }
+    }
+}
---
+++ ConsoleFileManager/FilesTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleFileManager
+{
+    class FilesTree
+    {
+        private DirectoryInfo Root { get; }
+        public List<File> Files {get; set;}
+
+        public FilesTree(DirectoryInfo root)
+        {
+            this.Root = root;
+        }
+    }
+}
